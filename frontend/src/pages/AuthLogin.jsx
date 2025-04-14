6cb3b9a86bd1307d23a6ed8134af71a68577bf8c
Zayaan Omar <zayaan.omar@icloud.com>
2025-04-14 16:56:31 +0200

    Removed unused variable AuthLogin.jsx

--- frontend/src/pages/AuthLogin.jsx
+++ frontend/src/pages/AuthLogin.jsx
@@ -3,7 +3,6 @@ import './AuthLogin.css';
 import { useNavigate } from 'react-router-dom';
 
 const AuthOptions = () => {
-  const navigate = useNavigate();
   const handleGoogleLogin = () => {
     // Placeholder for actual Google login logic
     console.log("Logging in with Google...");
