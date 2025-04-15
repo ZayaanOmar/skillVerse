ce2cdfad6a5ffd084ef62dcc893a64db1be23dbd
EbrahimHoosen <ebrahimhoosen09@gmail.com>
2025-04-15 23:01:33 +0200

    changed again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthLogin from "./pages/AuthLogin";
 import ClientHome from "./pages/ClientHome";
 import RoleSelection from "./pages/RoleSelection";
-import Navbar from "./components/Navbar";
+
 import FreelancerHome from "./pages/FreelancerHome";
 
 function App() {
   return (
     <Router>
-      <Navbar /> 
+      
       <Routes>
         <Route path="/" element={<AuthLogin />} />
         <Route path="/client/home" element={<ClientHome />} />
--- frontend/src/pages/ClientHome.jsx
+++ frontend/src/pages/ClientHome.jsx
@@ -3,7 +3,17 @@ import './ClientHome.css';
 
 const ClientHome = () => {
   return (
+    
     <main className="client-home">
+      <nav className="bg-slate-800 text-white px-8 py-4 flex justify-end items-center">
+    <ul className="flex gap-6 list-none m-0 p-0">
+      <li><a href="#home" className="font-semibold hover:underline">Home</a></li>
+      <li><a href="#features" className="font-semibold hover:underline">Features</a></li>
+      <li><a href="#about" className="font-semibold hover:underline">About</a></li>
+      <li><a href="#contact" className="font-semibold hover:underline">Contact</a></li>
+      <li><a href="#settings" className="font-semibold hover:underline">Settings</a></li>
+      </ul>
+    </nav>
       <section>
         {/* Welcome Banner */}
         <header className="welcome-banner">
@@ -14,7 +24,7 @@ const ClientHome = () => {
         <section className='chooseDesc'>
           <p className="description">CHOOSE A SERVICE</p>
         </section>
-
+    
         {/*each button */}
         <section className="section1">
           <button className="button1">Software Development</button>
