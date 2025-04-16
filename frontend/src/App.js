de82c3618d80da3366802332e828ff9d5d4fcb79
Zayaan Omar <zayaan.omar@icloud.com>
2025-04-16 21:10:30 +0200

    Added basic admin page placeholder and implemented admin route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthLogin from "./pages/AuthLogin";
 import ClientHome from "./pages/ClientHome";
 import RoleSelection from "./pages/RoleSelection";
-import JobRequests from './pages/JobRequests';
+import JobRequests from "./pages/JobRequests";
 import FreelancerHome from "./pages/FreelancerHome";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import AdminHome from "./pages/AdminHome";
 function App() {
   const user = JSON.parse(localStorage.getItem("user"));
 
   return (
     <Router>
-      
       <Routes>
         <Route path="/" element={<AuthLogin />} />
         <Route path="/client/home" element={<ClientHome />} />
         <Route path="/client/home" element={<ClientHome user={user} />} />
         <Route path="/roles" element={<RoleSelection />} />
-        <Route path="/freelancer/home" element={<FreelancerHome user={user} />} />
-        <Route path="/freelancer/jobs" element={<JobRequests />} /> 
+        <Route
+          path="/freelancer/home"
+          element={<FreelancerHome user={user} />}
+        />
+        <Route path="/freelancer/jobs" element={<JobRequests />} />
+        <Route path="/admin/home" element={<AdminHome />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/AdminHome.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./AdminHome.css";
+
+const AdminHome = () => {
+  return (
+    <>
+      <main className="admin-home">
+        <section>AdminHome</section>
+        <section>
+          <h1>This is a placeholder page for now.</h1>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default AdminHome;
